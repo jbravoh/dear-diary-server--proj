3885a7937fac7087567766ca141144627da6fdf7
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-09-29 22:58:07 +0100

    Build login route

--- routes/jwtAuth.ts
+++ routes/jwtAuth.ts
@@ -1,39 +1,78 @@
-import {Router} from "express";
-import { client }  from "../server" 
-import  bcrypt from "bcrypt";
+import { Router } from "express";
+import { client } from "../server";
+import bcrypt from "bcrypt";
 import jwtGenerator from "../utils/jwtGenerator";
 
-//registering 
+//REGISTER
 
-const router = Router()
+const router = Router();
 
 router.post("/register", async (req, res) => {
   try {
     //1. destructure the req.body(name, email. password)
 
-    const { username, email, password} = req.body;
+    const { username, email, password } = req.body;
     //2. check if user exists (if user exists then throw error)
 
-    const user = await client.query("SELECT * FROM users WHERE email = $1", [email]);
+    const user = await client.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
 
     if (user.rows.length !== 0) {
-        return res.status(401).send("User already exists")
+      return res.status(401).send("User already exists");
     }
+
     //3. Bcrypt the user password
 
     const saltRound = 10;
     const salt = await bcrypt.genSalt(saltRound);
-    const bcryptPassword = await bcrypt.hash(password, salt)
+    const bcryptPassword = await bcrypt.hash(password, salt);
 
-    //4. enter the user inside our database 
-    const newUser = await client.query("INSERT into users (username, email, password) VALUES ($1, $2, $3) RETURNING *", [username, email, bcryptPassword]);
-    
+    //4. enter the user inside our database
+    const newUser = await client.query(
+      "INSERT into users (username, email, password) VALUES ($1, $2, $3) RETURNING *",
+      [username, email, bcryptPassword]
+    );
+
     //5. generating our jwt token
 
-    const token = jwtGenerator(newUser.rows[0].user_id)
+    const token = jwtGenerator(newUser.rows[0].user_id);
 
-    res.json({token})
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+});
 
+// LOGIN
+
+router.post("/login", async (req, res) => {
+  try {
+
+    //1. destructure the req.body
+    const {email, password} = req.body;
+
+    //2. check if user doesn't exist (if not, we throw error)
+    const user = await client.query("SELECT * FROM users WHERE email = $1", [email]);
+
+    if (user.rows.length === 0) {
+        return res.status(401).json("Password or Email is incorrect")
+    }
+
+    //3. check if incoming password is the same as the db password
+
+    const validPassword = await bcrypt.compare(password, user.rows[0].password);
+    
+    if (!validPassword) {
+        return res.status(401).json("Password or Email is incorrect")
+    }
+ 
+    //4. give them the jwt token
+
+    const token = jwtGenerator(user.rows[0].user_id);
+
+    res.json({ token });
 
   } catch (error) {
     console.error(error);
