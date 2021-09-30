dc2912c30c343b0f911ae613cf2a1db111b10847
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-09-30 17:42:38 +0100

    Fix authorisation file error

--- middleware/authorisation.ts
+++ middleware/authorisation.ts
@@ -1,35 +1,34 @@
-import jsonwebtoken from "jsonwebtoken"
-import {Response, Request, NextFunction} from "express"
+import jsonwebtoken from "jsonwebtoken";
 
+import { Request } from "express";
 
+export interface RequestWithUser extends Request {
+  user: string;
+}
 
+const jwt = jsonwebtoken;
 
+export default async function authorisation(
+  req: RequestWithUser,
+  res: any,
+  next: any
+) {
+  const jwtToken = req.header("token");
 
-interface RequestWithUser extends Request {
- user: string
- }
-
-const jwt = jsonwebtoken
-
-
-
-export default async (req: any, res: Response, next: NextFunction) => {
-    try {
-        const jwtToken = req.header("token");
- 
-        if (!jwtToken) {
-            return res.status(403).json("Not Authorised")
-        }
-        const payload = jwt.verify(jwtToken, process.env.jwtSecret ) as jsonwebtoken.JwtPayload;
-
-        req.user = payload.user
-
-        
-
-        next()
-        
-    } catch (error) { 
-        console.error(error) 
-        return res.status(403).json("Not Authorised")
+  try {
+    if (!jwtToken) {
+      return res.status(403).json("Not Authorised");
     }
+    const payload = jwt.verify(
+      jwtToken,
+      process.env.JWT_SECRET
+    ) as jsonwebtoken.JwtPayload;
+
+    req.user = payload.user;
+
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.status(403).json("Not Authorised");
+  }
 }
