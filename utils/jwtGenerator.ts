bbf5d27f855605d45aa88d656d2617aae2d1afae
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-09-30 17:43:30 +0100

    Update user interface

--- utils/jwtGenerator.ts
+++ utils/jwtGenerator.ts
@@ -1,11 +1,12 @@
-import jwt from "jsonwebtoken"; 
+import jwt from "jsonwebtoken";
 // import { config } from "dotenv";
-import userInterface from "../userInterface";
+import userI from "../userInterface";
 
-export default function jwtGenerator(user_id:userInterface) {
-    const payload = {
-        user: user_id
-    }
+export default function jwtGenerator(user_id: userI[]) {
+  const payload = {
+    user: user_id,
+  };
 
-    return jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: "1hr"})
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1hr" });
 }
+
