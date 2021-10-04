d40236b9db2e5578d130c3f02a73da7a10da0a42
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-10-04 21:39:12 +0100

    Complete building the restful api

--- routes/dashboard.ts
+++ routes/dashboard.ts
@@ -11,15 +11,17 @@ router.get("/", authorisation, async (req: RequestWithUser, res) => {
   try {
     // req.user has the payload
 
+
+
     const user = await client.query(
-      "SELECT users.user_id, users.username, posts.post_id, posts.title, posts.content FROM users LEFT JOIN posts on users.user_id = posts.user_id WHERE users.user_id = $1",
+      "SELECT users.user_id, users.username, posts.post_id, posts.title, posts.content FROM users, posts WHERE users.user_id = $1",
       [req.user]
     );
 
     res.json(user.rows);
-  } catch (error) { 
+  } catch (error) {
     console.error(error);
-    res.status(500).json("Server error"); 
+    res.status(500).json("Server error");
   }
 });
 
@@ -29,47 +31,48 @@ router.post("/posts", authorisation, async (req: RequestWithUser, res) => {
   try {
     console.log(req.body);
     const { title, content } = req.body;
-    const newPost = await client.query("INSERT INTO posts (user_id, title, content) VALUES ($1, $2, $3) RETURNING *", [req.user, title, content])
-    res.json(newPost.rows[0])
+    const newPost = await client.query(
+      "INSERT INTO posts (user_id, title, content) VALUES ($1, $2, $3) RETURNING *",
+      [req.user, title, content]
+    );
+    res.json(newPost.rows[0]);
   } catch (error) {
-    console.error(error)
-    
+    console.error(error);
   }
 });
 
 router.put("/posts/:id", authorisation, async (req: RequestWithUser, res) => {
   try {
-    const { id } = req.params
-    const { title, content } = req.body
-    const updatePost = await client.query("UPDATE posts SET title = $1, content = $2  WHERE post_id = $3 AND user_id = $4", [title, content, id, req.user]);
+    const { id } = req.params;
+    const { title, content } = req.body;
+    await client.query(
+      "UPDATE posts SET title = $1, content = $2  WHERE post_id = $3 AND user_id = $4",
+      [title, content, id, req.user]
+    );
 
-    // if (updatePost.rows.length === 0) {
-    //   return res.json("This post is not yours")
-    // }
-
-    res.json("Post was updated")
+    res.json("Post was updated");
   } catch (error) {
-    console.error(error)
-  
+    console.error(error);
   }
-})
+});
 
-// delete a post 
+// delete a post
 
-router.delete("/posts/:id", authorisation, async (req: RequestWithUser, res) => {
-  try {
-    const { id } = req.params
-    const deletePost = await client.query("DELETE FROM posts WHERE post_id = $1 AND user_id = $2 RETURNING *", [id, req.user])
-    res.json("Post was deleted")
-  } catch (error) {
-    console.error(error)
+router.delete(
+  "/posts/:id",
+  authorisation,
+  async (req: RequestWithUser, res) => {
+    try {
+      const { id } = req.params;
+      await client.query(
+        "DELETE FROM posts WHERE post_id = $1 AND user_id = $2 RETURNING *",
+        [id, req.user]
+      );
+      res.json("Post was deleted");
+    } catch (error) {
+      console.error(error);
+    }
   }
-})
+);
 
-
-
-
-
-
- 
 export default router;
